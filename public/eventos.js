88475382fd3ec4fc3174ed0c336aa2ba5ab9bb5c
Yeico Med <yeicogm@gmail.com>
2024-12-22 12:38:14 +0000

    blob

--- public/eventos.js
+++ public/eventos.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
   // Obtener el elemento de entrada de la contraseña de login
   const loginPasswordInput = document.getElementById('login-password');
   const loginUserInput = document.getElementById('login-user');
+  const regUserInput = document.getElementById('reg-user')
+  const regPasswordInput = document.getElementById('reg-password')
+  var imgBlob = document.getElementById("imgBlob");
   // Función para despachar el evento personalizado
   const dispatchTextChangeEvent = (inputElement) => {
     inputElement.dispatchEvent(textChangeEvent);
@@ -14,15 +17,36 @@ document.addEventListener('DOMContentLoaded', () => {
   // Añadir oyente de eventos para el evento personalizado
   const addTextChangeListener = (inputElement) => {
     inputElement.addEventListener('textchange', () => {
-      console.log(`El evento textchange se ha disparado para ${inputElement.id}!`);
-      var formulario = document.getElementById("formReg");
+      //console.log(`El evento textchange se ha disparado para ${inputElement.id}!`);
+      var formularioReg = document.getElementById("formReg");
+      var formularioLogin = document.getElementById("formLogin");
   
       if (loginPasswordInput.value.length > 0 || loginUserInput.value.length > 0) {
-        formulario.style.display = "none";
+        formularioReg.style.display = "none";
       } else { 
-        formulario.style.display = "flex";
+        formularioReg.style.display = "flex";
+      }
+
+      if (regPasswordInput.value.length > 0 || regUserInput.value.length > 0) {
+        formularioLogin.style.display = "none";
+      } else { 
+        formularioLogin.style.display = "flex";
       }
      
+     
+      if (loginUserInput.value.length > 5 || regUserInput.value.length > 5) {
+         imgBlob.src = 'public/img/blob/4.jpg'; 
+      } else if (loginUserInput.value.length > 2 || regUserInput.value.length > 2) {
+          imgBlob.src = 'public/img/blob/3.jpg'; 
+      } else if (loginUserInput.value.length > 0 || regUserInput.value.length > 0) {
+          imgBlob.src = 'public/img/blob/2.jpg';
+       } else { 
+        imgBlob.src = 'public/img/blob/1.jpg'; 
+      } 
+      if (loginPasswordInput.value.length > 0 || regPasswordInput.value.length > 0){
+        imgBlob.src = 'public/img/blob/5.jpg';
+      }
+      
     });
 
     // Despachar el evento personalizado cuando el valor del input cambie
@@ -34,4 +58,6 @@ document.addEventListener('DOMContentLoaded', () => {
   // Añadir oyente de eventos para el input de contraseña de login
   if (loginPasswordInput) addTextChangeListener(loginPasswordInput);
   if (loginUserInput) addTextChangeListener(loginUserInput);
+  if (regUserInput) addTextChangeListener(regUserInput);
+  if (regPasswordInput) addTextChangeListener(regPasswordInput);
 });
